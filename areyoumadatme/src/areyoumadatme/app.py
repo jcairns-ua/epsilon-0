15c2b3565ea59b232eac8e580cf1a27c76a23d40
jcairns-ua <jcairns@ualberta.ca>
2023-11-18 15:11:03 -0700

    changed buttons

--- areyoumadatme/src/areyoumadatme/app.py
+++ areyoumadatme/src/areyoumadatme/app.py
@@ -12,30 +12,40 @@ class AreYouMadatMe(toga.App):
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
         name_label = toga.Label(
-            "Your name: ",
+            "Are You Mad at Me?",
             style=Pack(padding=(0, 5))
         )
-        self.name_input = toga.TextInput(style=Pack(flex=1))
+        #self.name_input = toga.TextInput(style=Pack(flex=1))
 
         name_box = toga.Box(style=Pack(direction=ROW, padding=5))
         name_box.add(name_label)
-        name_box.add(self.name_input)
+        #name_box.add(self.name_input)
 
-        button = toga.Button(
-            "Say Hello!",
-            on_press=self.say_hello,
+        login_button = toga.Button(
+            "Login",
+            on_press=self.login,
+            style=Pack(padding=5)
+        )
+
+        signup_button = toga.Button(
+            "Sign Up",
+            on_press=self.signup,
             style=Pack(padding=5)
         )
 
         main_box.add(name_box)
-        main_box.add(button)
+        main_box.add(login_button)
+        main_box.add(signup_button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
-    def say_hello(self, widget):
-        print(f"Hello, {self.name_input.value}")
+    def login(self, widget):
+        print(f"Hello")
+
+    def signup(self, widget):
+        print(f"Hello")
 
 
 def main():
