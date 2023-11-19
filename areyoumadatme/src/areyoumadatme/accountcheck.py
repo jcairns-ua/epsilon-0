ec7290ed4a06d9175ce31b6d103b98ba7b0d16df
jcairns-ua <jcairns@ualberta.ca>
2023-11-18 18:49:49 -0700

    wrote more account check

--- areyoumadatme/src/areyoumadatme/accountcheck.py
+++ areyoumadatme/src/areyoumadatme/accountcheck.py
@@ -1,4 +1,55 @@
 """
 checking/ addding accounts
+accounts.txt:
+name,username,email,password
 """
+
+def getaccounts():
+    """
+    opens accounts.txt, creates dictionary of all acounts
+    returns: none
+
+    accounts dict of form "username" : [name, email, password]
+    """
+    acctfile = open("accounts.txt", "r")
+    accounts = {}
+    for lines in acctfile:
+        account = lines.split(",")
+        username = account.pop(1)
+        accounts[username] = account
+
+    acctfile.close()
+
+    return accounts
+
+
+
+
+def addaccount(accounts, name, email, username, password):
+    """
+    adds account to dictionary + text file
+    cannot contain ','
+    username cannot be 'true'
+    returns:
+        1 : if added account successfully
+        0 : if account add failed
+    """
+    try:
+        #add account if username not already exists
+        accounts[username] = [name, email, password]
+
+
+    except KeyError:
+        #if in dictionary already
+        pass
+
+def checkaccount(username, password):
+    """
+    logs user in if username exists and password matches
+    returns: 
+        if valid : "true"
+        if username doesnt exist : username
+        if password invalid : "false"
+    """
+    pass
 
